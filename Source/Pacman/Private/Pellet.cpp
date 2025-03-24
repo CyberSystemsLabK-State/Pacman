ea01c1cb62e68d431f8b1404878993431f80f8ad
kprado <kprad7571@gmail.com>
2025-03-24 02:01:23 -0500

    feat: pellet actor added

---
+++ Source/Pacman/Private/Pellet.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pellet.h"
+
+// Sets default values
+APellet::APellet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SetActorEnableCollision(true);
+
+	BaseCollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("BaseCollisionComponent"));
+	PelletMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PelletMesh"));
+	PelletMesh->SetupAttachment(BaseCollisionComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> Sphere(TEXT("StaticMesh'/Script/Engine.StaticMesh'/Game/StarterContent/Props/MaterialSphere.MaterialSphere''"));
+	
+	if (Sphere.Succeeded()) {
+		PelletMesh->SetStaticMesh(Sphere.Object);
+		PelletMesh->SetWorldScale3D(FVector(0.3, 0.3, 0.3));
+
+		BaseCollisionComponent->SetSphereRadius(16);
+	}
+}
+
+/*
+// Called when the game starts or when spawned
+void APellet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APellet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+*/
+
---
+++ Source/Pacman/Public/Pellet.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Actor.h"
+#include "Pellet.generated.h"
+
+UCLASS()
+class PACMAN_API APellet : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this actor's properties
+	APellet();
+	UPROPERTY(EditDefaultsOnly, Category = "Collectable")
+	UStaticMeshComponent* PelletMesh;
+	UPROPERTY(EditDefaultsOnly, Category = "Collectable")
+	USphereComponent* BaseCollisionComponent;
+	UPROPERTY(EditAnywhere, Category = "Collectable")
+	bool isBluePill;
+	
+/*
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+*/
+};
